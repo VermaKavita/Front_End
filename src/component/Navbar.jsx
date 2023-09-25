b846812f3d7ee498f3fc778621f799102e661f8e
Kavita Verma <114670557+VermaKavita@users.noreply.github.com>
2023-09-25 12:22:51 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <div className='col-12-lg'>
     <nav className="navbar navbar-expand-lg blur border-radius-xl top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4" style={{backgroundColor:'#e3f2fd',borderRadius: '15px'}}>
   <div className="container-fluid">
-    <a className="navbar-brand font-weight-bolder ms-lg-0 ms-3" href="#">Student Portal</a>
+    <Link className="navbar-brand font-weight-bolder ms-lg-0 ms-3" to={"/"}>Student Portal</a>
     <button className="navbar-toggler" type="button" >
       
     </button>
